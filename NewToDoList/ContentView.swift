b2678a1346ea0cfdedfdd2f1f89739d4d8b7fe5b
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2024-05-18 04:41:34 -0400

    Complete Iteration 0

--- NewToDoList/ContentView.swift
+++ NewToDoList/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("To Do List")
+                     .font(.system(size: 40))
+                     .fontWeight(.black)
+                Spacer()
+                Button {
+                    withAnimation {
+                        self.showNewTask = true
+                    }
+                } label: {
+                    Text("+")
+                }
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
+        if showNewTask {
+            NewToDoView()
+        }
     }
 }
 
--- NewToDoList/NewToDoView.swift
+++ NewToDoList/NewToDoView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct NewToDoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Task title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                    .cornerRadius(12)
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                            Text("Is it important?")
+                        }
+            .font(.system(size: 25))
+            .fontWeight(.light)
+            Button {
+
+            } label: {
+                    Text("Save")
+            }
+        }
+        .padding()
     }
 }
 
