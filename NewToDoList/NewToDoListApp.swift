47e091c638c29ce92f45adec76f62cf44e90ad7a
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2024-05-18 04:17:26 -0400

    Initial Commit

---
+++ NewToDoList/NewToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewToDoListApp.swift
+//  NewToDoList
+//
+//  Created by Kiara on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NewToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
