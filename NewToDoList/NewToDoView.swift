1e1ec63855ef119f5e94d740059b6dbc8487563f
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2024-05-19 20:26:09 -0400

    Complete Iteration 1

--- NewToDoList/NewToDoView.swift
+++ NewToDoList/NewToDoView.swift
@@ -6,32 +6,46 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    @Bindable var toDoItem: ToDoItem
+    @Binding var showNewTask: Bool
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack {
             Text("Task title:")
                 .font(.title)
                 .fontWeight(.bold)
-            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
                 .padding()
                 .background(Color(.systemGroupedBackground))
                     .cornerRadius(12)
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                            Text("Is it important?")
+            Toggle(isOn: $toDoItem.isImportant) {
+                            Text("Is it important?").padding()
                         }
             .font(.system(size: 25))
             .fontWeight(.light)
             Button {
-
+                addToDo()
+                self.showNewTask = false
             } label: {
                     Text("Save")
             }
         }
         .padding()
     }
+    func addToDo() {
+        let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
+    }
 }
 
 #Preview {
-    NewToDoView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: ToDoItem.self, configurations: config)
+
+    let toDo = ToDoItem(title: "Example ToDo", isImportant: false)
+    return NewToDoView(toDoItem: toDo, showNewTask: .constant(true))
+        .modelContainer(container)
 }
